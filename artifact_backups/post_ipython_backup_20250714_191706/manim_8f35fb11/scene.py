4da6c35e8c536aa64a00e778a42e5abd86281452
Stanley Chisango <47616017+scooter-lacroix@users.noreply.github.com>
2026-03-12 07:20:35 -0400

    Chore: cleaup
    Artifacts

--- artifact_backups/post_ipython_backup_20250714_191706/manim_8f35fb11/scene.py
+++
@@ -1,8 +0,0 @@
-from manim import *
-
-class SquareToCircle(Scene):
-    def construct(self):
-        square = Square()
-        circle = Circle()
-        self.play(Transform(square, circle))
-        self.wait()
--- artifact_backups/test_backup_20250714_185501/manim_fea21845/scene.py
+++
@@ -1,6 +0,0 @@
-from manim import *
-class SquareToCircle(Scene):
-    def construct(self):
-        square = Square()
-        circle = Circle()
-        self.play(Transform(square, circle))
